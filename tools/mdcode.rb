cca91fa9643f9b9aeeaf6b267f955a2a2ac5b010
Stefan Kersten <kaoskorobase@gmail.com>
2018-12-27 23:00:03 +0100

    Add CMake build system

--- tools/mdcode.rb
+++ tools/mdcode.rb
@@ -1,16 +1,24 @@
 #!/usr/bin/env ruby
 
+if ARGV.size != 2
+  puts "Usage: #{File.basename($0)} INFILE OUTFILE"
+  exit 1
+end
+
 cpp = false
-while line = gets
-  if cpp
-    if /^~~~~$/ =~ line
-      cpp = false
+
+File.open(ARGV[1], "w") do |out|
+  File.open(ARGV[0]).each do |line|
+    if cpp
+      if /^~~~~$/ =~ line
+        cpp = false
+      else
+        out.write(line)
+      end
     else
-      puts line
-    end
-  else
-    if /^~~~~cpp$/ =~ line
-      cpp = true
+      if /^~~~~cpp$/ =~ line
+        cpp = true
+      end
     end
   end
 end
